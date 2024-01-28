1f13e7f1d515e100a8cdc15e70d815063ab04f9d
jake <jw00006@mix.wvu.edu>
2024-01-28 16:16:58 -0500

    Add project files.

---
+++ Homework1Williamson/Pages/SearchResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework1Williamson.Pages
+{
+    public class SearchResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
